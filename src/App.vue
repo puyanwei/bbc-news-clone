f39cae90a903e81cd91e93539320791fc71c8595
Puyan Wei <puyanwei@gmail.com>
2018-08-27 16:30:00 +0100

    Complete mock up of sections in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,140 @@
 <template>
-  <div id="app">
-      <h1>This is BBC News</h1>
-  </div>
+	<div id="app">
+		<div class="top-banner">
+			<h1>NEWS TOP LEFT</h1>
+			<button>Sections</button>
+		</div>
+		<div class="stories-tab">
+			<h3>Latest Stories</h3>
+			<h3>Most Read</h3>
+		</div>
+		<div class="main-story">
+			<img src="https://pbs.twimg.com/profile_images/839928218710855680/B6JS6Aip_400x400.jpg" alt="" />
+			<h1>HEADLINE</h1>
+			<h4>Subtext to headline</h4>
+			<h4>Time of article | Story category</h4>
+			<h3>Sub-headlines relating to story</h3>
+			<h3>Sub-headlines relating to story</h3>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="must-see">
+			<h1>video</h1>
+		</div>
+		<div class="watch-live"></div>
+		<div class="listen-live"></div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="most-watched">
+			<ul>
+				<li>1. Story One</li>
+				<li>2. Story Two</li>
+				<li>3. Story Three</li>
+				<li>4. Story Four</li>
+				<li>5. Story Five</li>
+			</ul>
+		</div>
+		<div class="featured-story">
+			<h1>IMAGE</h1>
+			<h3>Headline to Featured Story</h3>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="other-stories">
+			<img src="https://www.dierendokters.com/images/Katten/ouderdomsgebreken_kat.jpg" alt="" />
+			<h2>HEADLINE</h2>
+			<h4>Time of article | Story category</h4>
+		</div>
+		<div class="local-news">
+			<h1>Get the news that's local to you</h1>
+			<button>Find my location</button>
+			<h4>Enter a town, city or UK postcode</h4>
+		</div>
+		<div class="long-reads">
+			<h1>IMAGE</h1>
+			<h1>HEADLINE</h1>
+		</div>
+		<div class="around-the-UK">
+			<h2>Set a nation to see relevant news</h2>
+			<h3>List of nations</h3>
+		</div>
+		<div class="newsbeat">
+			<h1>IMAGE</h1>
+			<h1>HEADLINE</h1>
+		</div>
+		<div class="sport">
+			<div class="top-story">
+				<h1>IMAGE</h1>
+				<h3>HEADLINE</h3>
+			</div>
+			<div class="other-sports-stories">
+				<h1>IMAGE</h1>
+				<h3>HEADLINE</h3>
+			</div>
+			<div class="other-sports-stories">
+				<h1>IMAGE</h1>
+				<h3>HEADLINE</h3>
+			</div>
+			<div class="other-sports-stories">
+				<h1>IMAGE</h1>
+				<h3>HEADLINE</h3>
+			</div>
+			<div class="other-sports-stories">
+				<h1>IMAGE</h1>
+				<h3>HEADLINE</h3>
+			</div>
+		</div>
+		<div class="find-us-here">
+			<h1>Social Media Icons</h1>
+		</div>
+		<div class="sections-dropdown">
+			<h1>Dropdown menu for sections</h1>
+		</div>
+		<footer>
+			<h4>Terms of Use</h4>
+			<h4>Private Policy</h4>
+			<h4>Accessibility Help</h4>
+			<h4>Contact the BBC</h4>
+			<h4>Get Personalised Newsletters</h4>
+			<h4>About the BBC</h4>
+			<h4>Cookies</h4>
+			<h4>Parental Guidence</h4>
+		</footer>
+	</div>
 </template>
 
 <script>
@@ -18,5 +151,9 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+
+img {
+    max-width: 300px;
 }
 </style>
